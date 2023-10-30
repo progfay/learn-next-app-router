a01b86b2b85e89904980a1ceb0986d1b54b12602
progfay <mano_shunsuke@r.recruit.co.jp>
2023-10-30 18:24:54 +0900

    3. Fetch the specific invoice
    https://nextjs.org/learn/dashboard-app/mutating-data#3-fetch-the-specific-invoice

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,9 +1,14 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
+  const invoice = await fetchInvoiceById(id);
+  const customers = await fetchCustomers();
+
+  if (invoice === undefined) notFound();
 
   return (
     <main>
@@ -17,7 +22,7 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      {/* <Form invoice={invoice} customers={customers} /> */}
+      <Form invoice={invoice} customers={customers} />
     </main>
   );
 }
