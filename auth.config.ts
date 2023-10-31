195257878beea3b079051efbaa836368ad02dfe6
progfay <mano_shunsuke@r.recruit.co.jp>
2023-10-31 17:44:40 +0900

    remove unnecessary log

--- auth.config.ts
+++ auth.config.ts
@@ -7,10 +7,8 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      console.log({ auth, nextUrl });
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      console.log({ isLoggedIn, isOnDashboard });
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
