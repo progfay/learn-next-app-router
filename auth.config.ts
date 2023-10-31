df9ec359805bcfb08ac6c4a824d9953f451f0e58
progfay <mano_shunsuke@r.recruit.co.jp>
2023-10-31 17:31:39 +0900

    Setting up NextAuth.js
    https://nextjs.org/learn/dashboard-app/adding-authentication#setting-up-nextauthjs

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,23 @@
 import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
+  providers: [],
   pages: {
     signIn: '/login',
   },
-};
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      console.log({ auth, nextUrl });
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      console.log({ isLoggedIn, isOnDashboard });
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+} satisfies NextAuthConfig;
